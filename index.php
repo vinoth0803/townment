13f6f7a7edf7fa2a5b907d8f7d65b189fcbaa60b
Vinoth <vinothkrish0803@gmail.com>
2025-03-12 10:52:46 +0530

    Style updated

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>TOWNMENT - Login</title>
-  <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="stylesheet" href="style.css">
+  <!-- <script src="https://cdn.tailwindcss.com"></script> -->
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen bg-cover bg-center px-4" 
